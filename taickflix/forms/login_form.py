071a8f17c63bbce918c0336ad905a48c1b7d15fe
Tiago Saraiva <tiagosaraiva3122009@gmail.com>
2026-01-03 00:26:03 -0300

    ajuste

--- taickflix/forms/login_form.py
+++ taickflix/forms/login_form.py
@@ -6,18 +6,3 @@ import hashlib
 class LoginForm(forms.Form):
     email = forms.EmailField(label='E-mail')
     senha = forms.CharField(label='Senha', widget=forms.PasswordInput)
-
-    '''
-    def clean_email(self):
-        email = self.cleaned_data.get('email').strip()
-        if not Cliente.objects.filter(email=email).exists():
-            raise forms.ValidationError('Emial inválido.')
-        return email
-    
-    def clean_senha(self):
-        senha = self.cleaned_data.get('senha').strip()
-        hash_senha = hashlib.sha256(senha.encode()).hexdigest()
-        if not Cliente.objects.filter(senha=hash_senha):
-            raise forms.ValidationError('Senha inválida.')
-        return senha
-    '''
