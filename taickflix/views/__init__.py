92c07f3e8b32930a2c1b859de83147ff39ddd89f
Tiago Saraiva <tiagosaraiva3122009@gmail.com>
2026-01-19 01:11:43 -0300

    criando sistema de logout

--- taickflix/views/__init__.py
+++ taickflix/views/__init__.py
@@ -2,3 +2,4 @@ from taickflix.views.login_view import login_view
 from taickflix.views.home_view import home_view
 from taickflix.views.cadastro_view import cadastro_view
 from taickflix.views.confirm_email_view import confirm_email_view
+from taickflix.views.logout_view import logout_view
